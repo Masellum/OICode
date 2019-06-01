f7287d1d1143e4e65e7d85926de484d405175359
Masellum <Masellum@outlook.com>
2019-06-01 17:20:55 +0800

    Modified Luogu P4315 // unfinished

--- Luogu/P4315.cpp
+++ Luogu/P4315.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <cstdio>
 
 #define LC (root * 2)
@@ -8,7 +9,8 @@ const int MAXN = 100000 + 5;
 
 int n;
 int head[MAXN], to[MAXN * 2], next[MAXN * 2], edgeWeight[MAXN * 2], cnt;
-int vertexWeight[MAXN], fa[MAXN], dep[MAXN], size[MAXN], son[MAXN], top[MAXN], dfn[MAXN], pos[MAXN], ts;
+int vertexWeight[MAXN], fa[MAXN], dep[MAXN], size[MAXN], son[MAXN], top[MAXN],
+    dfn[MAXN], pos[MAXN], ts;
 
 void addEdge(int f, int t, int w) {
     to[++cnt] = t;
@@ -23,12 +25,13 @@ void dfs1(int u, int f) {
     size[u] = 1;
     son[u] = -1;
     for (int e = head[u], v = to[e]; e; e = next[e], v = to[e]) {
-        if (v == f) 
+        if (v == f)
             continue;
         dfs1(v, u);
         vertexWeight[v] = edgeWeight[e];
         size[u] += size[v];
-        if (son[u] == -1 || size[son[u]] < size[v]) son[u] = v;
+        if (son[u] == -1 || size[son[u]] < size[v])
+            son[u] = v;
     }
 }
 
@@ -36,7 +39,8 @@ void dfs2(int u, int t) {
     top[u] = t;
     dfn[u] = ++ts;
     pos[ts] = u;
-    if (son[u] == -1) return;
+    if (son[u] == -1)
+        return;
     dfs2(son[u], t);
     for (int e = head[u], v = to[e]; e; e = next[e], v = to[e]) {
         if (v == fa[u] || v == son[u])
@@ -49,9 +53,7 @@ struct SegmentTree {
     int max[MAXN * 4];
     long long tagAdd[MAXN * 4], tagCover[MAXN * 4];
 
-    void pushUp(int root) {
-        max[root] = std::max(max[LC], max[RC]);
-    }
+    void pushUp(int root) { max[root] = std::max(max[LC], max[RC]); }
 
     void build(int root, int l, int r) {
         if (l == r) {
@@ -64,6 +66,8 @@ struct SegmentTree {
         build(LC, l, mid);
         build(RC, mid + 1, r);
         pushUp(root);
+        tagAdd[root] = 0;
+        tagCover[root] = -1;
     }
 
     void add(int root, int l, int r, int d) {
@@ -78,6 +82,7 @@ struct SegmentTree {
     }
 
     void pushDown(int root, int l, int r) {
+        if (l == r) return;
         int mid = (l + r) / 2;
         if (tagCover[root] != -1) {
             cover(LC, l, mid, tagCover[root]);
@@ -94,6 +99,7 @@ struct SegmentTree {
     void modifyCover(int root, int l, int r, int left, int right, int x) {
         if (l == left && r == right) {
             cover(root, l, r, x);
+            return;
         }
         pushDown(root, l, r);
         int mid = (l + r) / 2;
@@ -109,8 +115,9 @@ struct SegmentTree {
     }
 
     void modifyAdd(int root, int l, int r, int left, int right, int d) {
-        if (l == left && ri == right) {
+        if (l == left && r == right) {
             add(root, l, r, d);
+            return;
         }
         pushDown(root, l, r);
         int mid = (l + r) / 2;
@@ -125,24 +132,28 @@ struct SegmentTree {
         pushUp(root);
     }
 
-    long long queryMax(int root, int l, int r, int left, int right) {
+    int queryMax(int root, int l, int r, int left, int right) {
         if (l == left && r == right) {
             return max[root];
         }
-        pushDown(root);
+        pushDown(root, l, r);
         int mid = (l + r) / 2;
         if (right <= mid) {
-            queryMax(LC, l, mid, left, right);
+            return queryMax(LC, l, mid, left, right);
         } else if (left > mid) {
-            queryMax(RC, mid + 1, r, left, right);
+            return queryMax(RC, mid + 1, r, left, right);
         } else {
-            queryMax(LC, l, mid, left, mid);
-            queryMax(RC, mid + 1, r, mid + 1, right);
+            return std::max(queryMax(LC, l, mid, left, mid),
+                            queryMax(RC, mid + 1, r, mid + 1, right));
         }
     }
 } tree;
 
 int main() {
+#ifndef ONLINE_JUDGE
+    freopen("./testdata/P4315/3.in", "r", stdin);
+    freopen("./testdata/P4315/3.out", "w", stdout);
+#endif
     scanf("%d", &n);
     for (int i = 1, u, v, w; i < n; ++i) {
         scanf("%d %d %d", &u, &v, &w);
@@ -154,19 +165,47 @@ int main() {
     tree.build(1, 1, n);
     char opt[7];
     scanf("%s", opt);
-    int u, v, w;
-    while (opt[0] != S) {
+    int u, v, k, w, ans = INT_MIN;
+    while (opt[0] != 'S') {
         scanf("%d %d", &u, &v);
         if (opt[0] == 'C') {
             if (opt[1] == 'h') {
-
+                k = u, w = v;
+                u = to[2 * k], v = to[2 * k + 1];
+                if (dep[u] < dep[v]) std::swap(u, v);
+                tree.modifyCover(1, 1, n, dfn[u], dfn[u], w);
             } else {
-
+                scanf("%d", &w);
+                while (top[u] != top[v]) {
+                    if (dep[top[u]] < dep[top[v]]) std::swap(u, v);
+                    tree.modifyCover(1, 1, n, dfn[top[u]], dfn[u], w);
+                    u = fa[top[u]];
+                }
+                if (dep[u] > dep[v]) std::swap(u, v);
+                if (u != v) tree.modifyCover(1, 1, n, dfn[u] + 1, dfn[v], w);
             }
         } else if (opt[0] == 'A') {
-
+            scanf("%d", &w);
+            while (top[u] != top[v]) {
+                if (dep[top[u]] < dep[top[v]]) std::swap(u, v);
+                tree.modifyAdd(1, 1, n, dfn[top[u]], dfn[u], w);
+                u = fa[top[u]];
+            }
+            if (dep[u] > dep[v]) std::swap(u, v);
+            tree.modifyAdd(1, 1, n, dfn[u], dfn[v], w);
+            tree.modifyAdd(1, 1, n, dfn[u], dfn[u], -w);
         } else {
-
+            ans = INT_MIN;
+            while (top[u] != top[v]) {
+                if (dep[top[u]] < dep[top[v]]) std::swap(u, v);
+                ans = std::max(ans, tree.queryMax(1, 1, n, dfn[top[u]], dfn[u]));
+                u = fa[top[u]];
+            }
+            if (dep[u] > dep[v]) std::swap(u, v);
+            if (u != v) ans = std::max(ans, tree.queryMax(1, 1, n, dfn[u] + 1, dfn[v]));
+            printf("%d\n", ans);
         }
+        scanf("%s", opt);
     }
+    return 0;
 }

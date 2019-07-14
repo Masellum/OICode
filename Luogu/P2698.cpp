d6c3eb860f756e19269b32115e2097724f4a4822
Masellum Guo <Masellum@outlook.com>
2019-07-14 20:57:55 +0800

    Add Luogu P2698.cpp // unfinished

---
+++ Luogu/P2698.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+
+const int MAXD = 1000000 + 5, MAXN = 100000 + 5;
+
+struct Drop {
+    int x, y;
+} drops[MAXN];
+
+int n, d;
+int maxQueue[MAXN], maxHead, maxTail, minQueue[MAXN], minHead, minTail, ans = INT_MAX;
+
+bool check(int w) {
+    memset(minQueue, 0, sizeof(minQueue));
+    memset(maxQueue, 0, sizeof(maxQueue));
+    maxHead = maxTail = minHead = minTail = 0;
+    minQueue[minTail++] = maxQueue[maxTail++] = 1;
+    for (int i = 2; i <= n; ++i) {
+        while (drops[i].y >= drops[maxQueue[maxTail - 1]].y && maxTail > maxHead) {
+            maxTail--;
+        }
+        maxQueue[maxTail++] = i;
+        while (drops[maxQueue[maxTail - 1]].x - drops[maxQueue[maxHead]].x + 1 > w) {
+            maxHead++;
+        }
+        while (drops[i].y <= drops[minQueue[minTail - 1]].y && minTail > minHead) {
+            minTail--;
+        }
+        minQueue[minTail++] = i;
+        while (drops[minQueue[minTail - 1]].x - drops[minQueue[minHead]].x + 1 > w) {
+            minHead--;
+        }
+        if (drops[maxQueue[maxHead]].y - drops[minQueue[minHead]].y >= w) return true;
+    }
+    return false;
+}
+
+int main() {
+    scanf("%d %d", &n, &d);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d %d", &drops[i].x, &drops[i].y);
+    }
+    std::sort(drops + 1, drops + n + 1, [](const Drop &a, const Drop &b) -> bool {
+        return a.x < b.x;
+    });
+    int l = 1, r = drops[n].x - drops[1].x + 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (check(mid)) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    printf("%d", ans == INT_MAX ? -1 : ans);
+    return 0;
+}

fa040275e1289cf63bedc28332f39d167509c16d
Masellum <Masellum@outlook.com>
2019-11-09 14:43:16 +0800

    Finish Luogu P3386

--- Luogu/P3386.cpp
+++ Luogu/P3386.cpp
@@ -1,82 +1,42 @@
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 
-const int MAXN = 1000  * 2 + 5;
+const int MAXN = 1000 + 5;
 
-struct Edge;
-struct Node;
+int n, m, e;
+int head[MAXN], to[MAXN * MAXN], next[MAXN * MAXN], cnt;
+int f[MAXN], ans;
+bool vis[MAXN];
 
-struct Node {
-    Edge *head;
-    int pos;
-    bool matched;
-} node[MAXN];
-
-struct Edge {
-    Node *from, *to;
-    Edge *next;
-    bool matched;
-
-    Edge() {}
-    Edge(Node *from, Node *to)
-        : from(from), to(to), next(from->head), matched(false) {}
-};
-
-void addEdge(int from, int to) {
-    node[from].head = new Edge(&node[from], &node[to]);
-    node[to].head = new Edge(&node[to], &node[from]);
+void addEdge(int u, int v) {
+    to[++cnt] = v;
+    next[cnt] = head[u];
+    head[u] = cnt;
 }
 
-int n, m, am, e;
-bool matched[MAXN];
-
-bool dfs(Node *u, Node *father, bool inMatch) {
-    bool flag = false;
-    for (auto e = u->head; e; e = e->next) {
-        if (e->matched == inMatch) {
-            continue;
-        }
-        auto v = e->to;
-        if (v == father) {
-            continue;
-        }
-        if (!v->matched || dfs(v, u, e->matched)) {
-            v->matched &= 1;
-            e->matched ^= 1;
-            flag = true;
-            break;
+bool dfs(int u) {
+    for (int e = head[u], v = to[e]; e; e = next[e], v = to[e]) {
+        if (vis[v]) continue;
+        vis[v] = true;
+        if (!f[v] || dfs(v)) {
+            f[v] = u;
+            return true;
         }
     }
-    return flag;
-}
-
-int hungarian() {
-    int ans = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (!node[i].matched) {
-            if (dfs(&node[i], new Node(), true)) {
-                ++ans;
-                matched[i] = true;
-            }
-        }
-    }
-    return ans;
+    return false;
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("./testdata/P3386.in", "r", stdin);
-    freopen("./testdata/P3386.out", "w", stdout);
-#endif
-    scanf("%d %d, %d", &n, &m, &e);
+    scanf("%d %d %d", &n, &m, &e);
     for (int i = 1, u, v; i <= e; ++i) {
         scanf("%d %d", &u, &v);
-        if (v > m) {
-            continue;
-        }
-        addEdge(u, v + n);
-        am++;
+        if (u <= n && v <= m) addEdge(u, v + n);
     }
-    printf("%d", hungarian());
+    for (int i = 1; i <= n; ++i) {
+        memset(f, 0, sizeof(f));
+        if (dfs(i)) ans++;
+    }
+    printf("%d", ans);
     return 0;
 }
